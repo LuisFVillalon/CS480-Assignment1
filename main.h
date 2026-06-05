6b0eb95fab900518bc376acdf78b726bd49e1c47
bravobvk <bravobvk@gmail.com>
2026-06-05 14:55:27 -0700

    Added header to main.h
    Added header file documentation and team member details.

--- main.h
+++ main.h
@@ -1,3 +1,16 @@
+/*
+CS 480 - Operating Systems [2026 Summer Session]
+Project: Assigment 1 - Chatbots with Threads & Semaphores
+File: main.h
+
+Team Members:
+- Luis Villalon: cssc3169 
+- Bobby Bavongkhoun: cssc3110
+
+Description:
+This header file contains the declarations, and definitions needed by the main.c, including the semaphore, shared variables, and thread function prototype.
+*/
+
 #ifndef BOTS_H
 #define BOTS_H
 
